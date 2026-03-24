4dd6a3928ea9c374426677f8515257a9b9ab8e26
Your Name <2307274697@qq.com>
2026-03-23 22:39:33 -0400

    从远程仓库移除大文件夹，本地保留

--- 备份3.21/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataset import collate_fn, PointOdysseyDataset
-# from .video_dataset import VideoDataset, KubricDataset, SintelDataset, ScanNetDataset, PointOdysseyDataset
-# from .augmentations import VideoAugmentation, TemporalSubsampling, AugmentationConfig
--- 备份3.21/losses/__init__.py
+++
@@ -1,3 +0,0 @@
-from .losses import D4RTLoss, compute_3d_loss, compute_2d_loss, compute_visibility_loss
-
-__all__ = ['D4RTLoss', 'compute_3d_loss', 'compute_2d_loss', 'compute_visibility_loss']
